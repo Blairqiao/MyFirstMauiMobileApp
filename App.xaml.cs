9b9bd51b061b7b3d6885844fbb42a5a05d55c680
Blairqiao <yanzhe.qiao.1@gmail.com>
2025-09-16 16:13:43 -0500

    added stacklayout

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFirstMauiMobileApp
+﻿using MyFirstMauiMobileApp.View;
+
+namespace MyFirstMauiMobileApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new MainPage();
         }
     }
 }
