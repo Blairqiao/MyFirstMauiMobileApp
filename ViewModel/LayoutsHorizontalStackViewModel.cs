df05af9404071b05d98e2dcc5f7cec239edacf56
Blairqiao <yanzhe.qiao.1@gmail.com>
2025-10-28 16:16:10 -0500

    update

--- ViewModel/LayoutsHorizontalStackViewModel.cs
+++ ViewModel/LayoutsHorizontalStackViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using MyFirstMauiMobileApp.Model.Titles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MyFirstMauiMobileApp.ViewModel
 {
